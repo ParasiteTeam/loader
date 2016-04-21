e659bfdcc2dcc25a8e4d8ec70e7e5f3ffaf33caf
Alex Zielenski <alexzielenski@gmail.com>
2016-04-21 16:12:09 -0400

    callback on a separate thread

--- ParasiteLoader/runtime.c
+++ ParasiteLoader/runtime.c
@@ -8,22 +8,29 @@
 
 #include "runtime.h"
 static CFMutableArrayRef callbacks;
+static dispatch_queue_t callback_queue = NULL;
 
 void PSNotify(CFURLRef path, CFIndex idx, CFIndex total) {
     if (callbacks == NULL) return;
     
-    CFIndex i = 0;
-    CFIndex tot = CFArrayGetCount(callbacks);
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        callback_queue = dispatch_queue_create("com.parasite.loader.callbacks.queue", NULL);
+    });
     
-    for (i = 0; i < tot; i++) {
-        PSLoaderCallback cb = CFArrayGetValueAtIndex(callbacks, i);
-        cb(path, idx, total);
-    }
-    
-    if (idx == total - 1 && callbacks != NULL) {
-        CFRelease(callbacks);
-        callbacks = NULL;
-    }
+    dispatch_async(callback_queue, ^{
+        CFIndex i = 0;
+        CFIndex tot = CFArrayGetCount(callbacks);
+        for (i = 0; i < tot; i++) {
+            PSLoaderCallback cb = CFArrayGetValueAtIndex(callbacks, i);
+            cb(path, idx, total);
+        }
+        
+        if (idx == total - 1 && callbacks != NULL) {
+            CFRelease(callbacks);
+            callbacks = NULL;
+        }        
+    });
 }
 
 void _PSRegisterCallback(PSLoaderCallback callback) {
