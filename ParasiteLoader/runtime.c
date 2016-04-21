8e153c25b2b1ad15fccb7e1079fd0914d828a33c
Alex Zielenski <alexzielenski@gmail.com>
2016-04-21 16:24:41 -0400

    synchronize queue registration

--- ParasiteLoader/runtime.c
+++ ParasiteLoader/runtime.c
@@ -8,17 +8,20 @@
 
 #include "runtime.h"
 static CFMutableArrayRef callbacks;
-static dispatch_queue_t callback_queue = NULL;
 
-void PSNotify(CFURLRef path, CFIndex idx, CFIndex total) {
-    if (callbacks == NULL) return;
-    
+static dispatch_queue_t callback_queue() {
+    static dispatch_queue_t callback_queue = NULL;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         callback_queue = dispatch_queue_create("com.parasite.loader.callbacks.queue", NULL);
     });
+    return callback_queue;
+}
+
+void PSNotify(CFURLRef path, CFIndex idx, CFIndex total) {
+    if (callbacks == NULL) return;
     
-    dispatch_async(callback_queue, ^{
+    dispatch_async(callback_queue(), ^{
         CFIndex i = 0;
         CFIndex tot = CFArrayGetCount(callbacks);
         for (i = 0; i < tot; i++) {
@@ -36,11 +39,13 @@ void PSNotify(CFURLRef path, CFIndex idx, CFIndex total) {
 }
 
 void _PSRegisterCallback(PSLoaderCallback callback) {
-    if (callbacks == NULL) {
-        callbacks = CFArrayCreateMutable(kCFAllocatorDefault,
-                                         0,
-                                         NULL);
-    }
-    
-    CFArrayAppendValue(callbacks, callback);
+    dispatch_async(callback_queue(), ^{
+        if (callbacks == NULL) {
+            callbacks = CFArrayCreateMutable(kCFAllocatorDefault,
+                                             0,
+                                             NULL);
+        }
+        
+        CFArrayAppendValue(callbacks, callback);
+    });
 }
